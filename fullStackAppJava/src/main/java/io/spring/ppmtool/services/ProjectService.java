ef727f5d9b94820f4a41a8626820eb3194686401
Tatiana Korkach <tatiana.korkach@sixt.com>
2019-08-29 13:31:03 +0200

    database with first create request

---
+++ fullStackAppJava/src/main/java/io/spring/ppmtool/services/ProjectService.java
@@ -0,0 +1,19 @@
+package io.spring.ppmtool.services;
+
+import io.spring.ppmtool.domain.Project;
+import io.spring.ppmtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project){
+        return projectRepository.save(project);
+    }
+
+
+}
