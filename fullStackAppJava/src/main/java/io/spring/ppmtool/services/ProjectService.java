436a9eae7e5f8073cc9fb73d8d50537ab096ed8e
Tatiana Korkach <tatiana.korkach@sixt.com>
2019-08-30 15:25:30 +0200

    added customized exception for project id

--- fullStackAppJava/src/main/java/io/spring/ppmtool/services/ProjectService.java
+++ fullStackAppJava/src/main/java/io/spring/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package io.spring.ppmtool.services;
 
 import io.spring.ppmtool.domain.Project;
+import io.spring.ppmtool.exceptions.ProjectIdException;
 import io.spring.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,9 +12,12 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
-    public Project saveOrUpdateProject(Project project){
-        return projectRepository.save(project);
+    public Project saveOrUpdateProject(Project project) {
+        try {
+            project.getProjectIdentifier().toUpperCase();
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID " + project.getProjectIdentifier().toUpperCase() + " already exists");
+        }
     }
-
-
 }
