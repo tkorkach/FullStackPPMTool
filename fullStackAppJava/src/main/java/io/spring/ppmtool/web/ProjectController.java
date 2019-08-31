b18f6e6e3671b5e50bd979e4d37dcf93ea457096
Tatiana Korkach <tatiana.korkach@sixt.com>
2019-08-31 11:52:43 +0200

    implemented get methods

--- fullStackAppJava/src/main/java/io/spring/ppmtool/web/ProjectController.java
+++ fullStackAppJava/src/main/java/io/spring/ppmtool/web/ProjectController.java
@@ -9,10 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.HashMap;
@@ -37,5 +34,17 @@ public class ProjectController {
 
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{projectId}")
+    public ResponseEntity<?> getProjectById(@PathVariable String projectId){
+        Project project = projectService.findProjectByIdentifier(projectId);
+        return new ResponseEntity<Project>(project, HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<?> getAllProjects(){
+        Iterable<Project> projects = projectService.findAllProjects();
+        return new ResponseEntity<Iterable<Project>>(projects, HttpStatus.OK);
     }
 }
