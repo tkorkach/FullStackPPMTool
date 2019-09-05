902b8168a48b8d6bd9bd90d27b6c810bde72b851
Tatiana Korkach <tatiana.korkach@sixt.com>
2019-09-05 16:39:21 +0200

    deleted unused imports

--- fullStackAppJava/src/main/java/io/spring/ppmtool/repositories/ProjectRepository.java
+++ fullStackAppJava/src/main/java/io/spring/ppmtool/repositories/ProjectRepository.java
@@ -4,7 +4,6 @@ import io.spring.ppmtool.domain.Project;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 
 @Repository
 public interface ProjectRepository extends CrudRepository<Project, Long> {
